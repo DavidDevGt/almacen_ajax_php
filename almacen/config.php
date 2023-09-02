9da998faa7881251f5a4d273fe11b025de4b158c
Davidvl <josuedavidvl18@gmail.com>
2023-09-01 21:17:46 -0600

    conexion bbdd

---
+++ almacen/config.php
@@ -0,0 +1,7 @@
+<?php 
+
+$conn = new mysqli("localhost", "root", "password", "almacen");
+
+if ($conn->connect_error){
+    die("Error conectando la base de datos... " . $conn->connect_error);
+}
---
+++ almacen/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Almacen</title>
+</head>
+<body>
+    <h3>Empleados</h3>
+
+    
+</body>
+</html>
